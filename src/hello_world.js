8c6d901f84817a9b979cecf8410fa6e56efc7ca6
yoko <yoko@transposit.com>
2019-02-28 23:07:09 +0000

    Commit from Transposit IDE

--- src/hello_world.js
+++ src/hello_world.js
@@ -1,69 +1,9 @@
 (params) => {
-	const _ = require('underscore.js');
-  	let containersInUse = [];
-    let dateNow = new Date();
-  	const TWO_WEEKS_BEFORE = dateNow.setDate(dateNow.getDate() - 14);
-  	let demoResult = api.run("this.get_containers_and_tasks_in_use", {env: "demo"});
-    containersInUse = _.unique(containersInUse.concat(demoResult[0]));
-  	let imagesToKeep = [];
-  	api.log("repo = " + params.repo);
-  	api.log("we are keeping these containers:");
-	console.log(containersInUse)
-	const regex = /([0-9]+)\..+\/([a-zA-Z0-9]+):(.+)/;
-  
-  	imagesToKeep = _.compact(_.unique(containersInUse.map((c) => {
-  		let captureGroups = regex.exec(c);
-        if (!captureGroups || captureGroups.length == 0) {
-             // Not one of our images. Consider it up to date
-            return "";
-        } else {
-          	let registryId = captureGroups[1];
-          	let repositoryName = captureGroups[2];
-          	let imgTag = captureGroups[3];
-          	return imgTag;
-        }
-     })));
-    console.log(imagesToKeep);
-
-  	let imagesToDelete = {};
-  	const repos = [params.repo];
-  	repos.forEach((rp) => {
-      	if (!_.contains(_.keys(imagesToDelete), rp)) {
-        	imagesToDelete[rp] = [];
-        }
-      
-    	let images = api.run("aws_ecr.list_images", {repositoryName: rp})[0]['imageIds'];
-
-      	images.forEach((img) => {
-          	const imgTag = img['imageTag'];
-          	if (!imgTag){
-            	return
-            }
-          	// check if it's being used
-        	if (_.contains(imagesToKeep, imgTag) || imgTag == "latest") {
-            	return;
-            }
-          
-          	// filter out images that do not match tag provided
-          	if (params.tagFilter && !imgTag.startsWith(params.tagFilter)) {
-              return;
-            }
-          
-            // for customization: if you encode commit sha in your image
-          	//
-            // if (imgTag.startsWith("ci_deploy")) {
-            //     const sha = imgTag.split("-")[1];
-            //       let commitInfo = api.run("this.get_commit", {sha: sha});
-            //     let commitTime = new Date(commitInfo[0]['commit']['author']['date']).getTime();
-            //       if (commitTime < TWO_WEEKS_BEFORE) {
-            //         imagesToDelete[rp].push(img);
-            //     };
-            //   return;
-            // }
-        });
-      console.log(images)
-    });
-  
+	const imagesToDelete = api.run("this.delete_old_images_dryrun", 
+                                   {repo: params.repo, 
+                                    tagFilter: params.tagFilter, 
+                                    gitRepositoryName: params.gitRepositoryName,
+                                   	gitRepositoryOwner: params.gitRepositoryOwner});
     let batchDeleteRequest = {"imageIds": [], 
                               "repositoryName": params.repo};
     imagesToDelete[params.repo].forEach((imgPair) => {
