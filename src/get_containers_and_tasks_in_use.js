e0ef6e684d43d504515c04104a23e2b4a70b5ce3
yoko <yoko@transposit.com>
2019-04-04 17:18:38 +0000

    Commit from Transposit IDE

--- src/get_containers_and_tasks_in_use.js
+++ src/get_containers_and_tasks_in_use.js
@@ -4,37 +4,7 @@
     const list_services = "aws_ecs.list_services";
     const describe_services = "aws_ecs.describe_services";
   	const describe_tasks_definition = "aws_ecs.describe_task_definition";
-
-	let clusters = api.run(list_clusters);
-
-  	clusters = clusters.map(function(cluster) {
-    	return cluster.split("/")[1]
-    });
-	
-  	
-  	// services indexed by cluster name
-  	const indexedServices = {};
- 
-  	clusters.forEach(function(c) {
-    	var resultSvcs = api.run(list_services, {
-        	cluster: c
-        });
-      	indexedServices[c] = resultSvcs;
-    });
-  
-  	// get task arns
-    let tasksInUse = [];
-  	_.each(indexedServices, function(val, key) {
-      	const svcNames = val.map(function(v) {
-        	return v.split("/")[1];
-        });
-		const tasks = api.run(describe_services, {
-            cluster: key,
-            services: svcNames
-        });
-      	tasksInUse.push(_.pluck(tasks[0].services, "taskDefinition"));
-    });
-  	tasksInUse = _.flatten(tasksInUse)
+	const tasksInUse = api.run("this.get_tasks_in_use");
   
   	  	// find containers that are in use, based on tasksInUse
   	const containersInUse = _.flatten(tasksInUse.map((tsk) => {
